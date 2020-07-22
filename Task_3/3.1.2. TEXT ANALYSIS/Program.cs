c3f76d0a6dcf3a49ef982e575995ebc01e145fa1
Serby1 <60844561+Serby1@users.noreply.github.com>
2020-07-23 00:16:25 +0400

    add 3.1.2

--- Task_3/3.1.2. TEXT ANALYSIS/Program.cs
+++ Task_3/3.1.2. TEXT ANALYSIS/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace _3._1._2._TEXT_ANALYSIS
 {
@@ -6,7 +7,15 @@ namespace _3._1._2._TEXT_ANALYSIS
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = "C:/Users/Vlad/source/EPAM/Task_3/3.1.2. TEXT ANALYSIS/fileIn.txt";
+            using (StreamReader fileIn = new StreamReader(path))
+            {
+                string str = fileIn.ReadToEnd();
+                Console.WriteLine("Текст из файла проанализирован...\n");
+                Analyzer analiz = new Analyzer(str);
+
+            }
+            Console.ReadLine();
         }
     }
 }
